a193adb086f33094b301f7b5503434696aa3ae01
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-10-12 13:28:50 +0300

    Add key to tabs

--- src/components/Tabbed.jsx
+++ src/components/Tabbed.jsx
@@ -8,18 +8,19 @@ export default function Tabbed({content}) {
 
     return (
         <div>
-        <div className="tabs">
-            <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
-            <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
-            <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
-            <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
-        </div>
+            <div className="tabs">
+                <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
+                <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
+                <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
+                <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+            </div>
 
-        {activeTab <= 2 ? (
-            <TabContent item={content.at(activeTab)} />
-        ) : (
-            <DifferentContent />
-        )}
+            {activeTab <= 2 ? (
+                // different key so each tab has different state, and will re-render and the likes will be different
+                <TabContent item={content.at(activeTab)} key={content.at(activeTab).summary} />
+            ) : (
+                <DifferentContent />
+            )}
         </div>
     );
 }
